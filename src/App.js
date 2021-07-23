ad6e223c1e837085ed547ade3acb9940e76d60c4
Shamany4 <anton-shamov99@mail.ru>
2021-07-23 20:34:57 +0700

    initial project && done header section

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import 'normalize.css';
+import 'macro-css';
+import './style.sass';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+
+        <Header />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import calendarIcons from '../assets/icons/calendar.svg';
+
+const Header = () => {
+  return (
+    <header className="header d-flex justify-between align-center">
+      <div className="header_line"></div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav__item"><a href="#" className="nav__link">Предложение</a></li>
+          <li className="nav__item"><a href="#" className="nav__link">О нас</a></li>
+          <li className="nav__item"><a href="#" className="nav__link">Консультация</a></li>
+        </ul>
+      </nav>
+      <div className="calendar calendar-widget">
+        <div className="d-flex align-end">
+          <div className="calendar-image">
+            <img src={calendarIcons} alt="Calendar icon" className="calendar-image__icon" />
+          </div>
+          <h3 className="calendar__title">Внести в календарь</h3>
+        </div>
+        <p className="calendar__desc"><span>Сейчас</span> самое время —
+          Завтра будет поздно!</p>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
