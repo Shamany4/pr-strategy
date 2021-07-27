1d97c99203ff98753166c97d6ce60f4bbc4afcf8
Shamany4 <anton-shamov99@mail.ru>
2021-07-27 16:27:36 +0700

    done experience section

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-
         <Header />
-
         <section className="home">
           <h1 className="section-title home__title">Получите работающую PR-стратегию</h1>
           <div className="d-flex fkex-column align-start">
@@ -43,8 +41,26 @@ function App() {
             </div>
           </div>
         </section>
-
       </div>
+      <section className="experience experience__section">
+        <div className="experience_rectangle"></div>
+        <div className="container experience__content">
+          <h2 className="section-title experience__title">По нашему опыту,</h2>
+          <div className="d-flex">
+            <p className="section-subtitle experience__text">По нашему опыту, компании теряют от восьми месяцев и миллиона рублей прежде, чем найдут правильный путь в пиаре и коммуникациях. <br /> И это без учёта «чёрных лебедей»!</p>
+            <div className="experience-card">
+              <p className="section-subtitle experience-card__text">Хорошая новость в том, <br /> что и общий вектор, <br /> и понятный алгоритм <br /> действий, и даже <br /> антикризисный менеджмент <br /> можно проработать заранее!</p>
+              <h3 className="experience-card__title">Что мы вместе <br /> с вами создадим?</h3>
+              <div className="experience-warning">
+                <span>!</span>
+              </div>
+              <div className="arrow-circle experience-card_circle">
+                <img src={whiteArrowIcon} alt="White arrow icon" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
