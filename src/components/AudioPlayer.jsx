8e123b323300f5287a12507822e36702e4014820
Shamany4 <anton-shamov99@mail.ru>
2021-09-13 18:08:01 +0700

    change playern and image in audio cases

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 const AudioPlayer = () => {
   return (
-    <iframe
-      frameBorder="0"
-      style={{ border: 'none' }}
-      title="player"
-      src="https://music.yandex.ru/iframe/#playlist/shamanyyy4/1000"
-    >Слушайте <a
-      href='https://music.yandex.ru/users/shamanyyy4/playlists/1000'>Player</a> — <a
-        href='https://music.yandex.ru/users/shamanyyy4'>shamanyyy4</a> на Яндекс.Музыке
+    <iframe frameBorder="0"
+            style={{ border: 'none' }}
+            title="The Business Pill"
+            src="https://music.yandex.ru/iframe/#playlist/im@follow-up.su/1000"
+    >Слушайте
+      <a
+        href='https://music.yandex.ru/users/im@follow-up.su/playlists/1000'>The Business Pill</a> —
+      <a
+        href='https://music.yandex.ru/users/im@follow-up.su'>Игорь Малинин</a> на Яндекс.Музыке
     </iframe>
   )
 }
